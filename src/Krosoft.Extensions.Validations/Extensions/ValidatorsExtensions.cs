45ce57cdba0ef5e4ab1f02460d4a4c51f8611e61
Kévin ROBERT <kevin.robert.kr@gmail.com>
2024-02-19 23:47:38 +0100

    Ajout ValidateMoreAsync

--- src/Krosoft.Extensions.Validations/Extensions/ValidatorsExtensions.cs
+++ src/Krosoft.Extensions.Validations/Extensions/ValidatorsExtensions.cs
@@ -6,8 +6,8 @@ namespace Krosoft.Extensions.Validations.Extensions;
 public static class ValidatorsExtensions
 {
     public static async Task ValidateMoreAndThrowAsync<T>(this IEnumerable<IValidator<T>> validators,
-                                                      T item,
-                                                      CancellationToken cancellationToken)
+                                                          T item,
+                                                          CancellationToken cancellationToken)
     {
         var failures = await validators.ValidateMoreAsync(item, cancellationToken);
         if (failures.Any())
@@ -17,8 +17,8 @@ public static class ValidatorsExtensions
     }
 
     public static async Task<ISet<string>> ValidateMoreAsync<T>(this IEnumerable<IValidator<T>> validators,
-                                                            T request,
-                                                            CancellationToken cancellationToken)
+                                                                T request,
+                                                                CancellationToken cancellationToken)
     {
         var failures = new List<string>();
         foreach (var validator in validators)
@@ -27,5 +27,15 @@ public static class ValidatorsExtensions
         }
 
         return failures.ToHashSet();
+    }
+
+    public static async Task ValidateMoreAsync<T>(this IEnumerable<IValidator<T>> validators,
+                                                  T request,
+                                                  Action<ISet<string>> action,
+                                                  CancellationToken cancellationToken)
+    {
+        var failures = await validators.ValidateMoreAsync(request, cancellationToken);
+
+        action(failures);
     }
 }
