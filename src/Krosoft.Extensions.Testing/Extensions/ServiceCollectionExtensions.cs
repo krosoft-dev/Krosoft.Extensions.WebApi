9de39253350f90d5c4fe23fe19ab54560c4d0a92
Kévin ROBERT <kevin.robert.kr@gmail.com>
2025-03-27 10:30:29 +0100

    Refactoring ServiceCollectionExtensions removal methods to return IServiceCollection for better chaining

--- src/Krosoft.Extensions.Testing/Extensions/ServiceCollectionExtensions.cs
+++ src/Krosoft.Extensions.Testing/Extensions/ServiceCollectionExtensions.cs
@@ -4,22 +4,21 @@ namespace Krosoft.Extensions.Testing.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void RemoveService(this IServiceCollection services, Func<ServiceDescriptor, bool> filter)
+    public static IServiceCollection RemoveService(this IServiceCollection services, Func<ServiceDescriptor, bool> filter)
     {
         var descriptor = services.SingleOrDefault(filter);
         if (descriptor != null)
         {
             services.Remove(descriptor);
         }
+
+        return services;
     }
 
-    public static void RemoveServices<T>(this IServiceCollection services)
-    {
-        services.RemoveServices(d => d.ServiceType == typeof(T));
-    }
+    public static IServiceCollection RemoveServices<T>(this IServiceCollection services) => services.RemoveServices(d => d.ServiceType == typeof(T));
 
-    public static void RemoveServices(this IServiceCollection services,
-                                      Func<ServiceDescriptor, bool> filter)
+    public static IServiceCollection RemoveServices(this IServiceCollection services,
+                                                    Func<ServiceDescriptor, bool> filter)
     {
         var servicesDescriptor = services.Where(filter).ToList();
         if (servicesDescriptor.Any())
@@ -29,15 +28,19 @@ public static class ServiceCollectionExtensions
                 services.Remove(serviceDescriptor);
             }
         }
+
+        return services;
     }
 
-    public static void RemoveTransient<TService>(this IServiceCollection services)
+    public static IServiceCollection RemoveTransient<TService>(this IServiceCollection services)
     {
         var serviceDescriptors = services.Where(x => x.ServiceType == typeof(TService) && x.Lifetime == ServiceLifetime.Transient).ToList();
         foreach (var serviceDescriptor in serviceDescriptors)
         {
             services.Remove(serviceDescriptor);
         }
+
+        return services;
     }
 
     /// <summary>
@@ -46,17 +49,19 @@ public static class ServiceCollectionExtensions
     /// <typeparam name="TService">The type of service interface which needs to be placed.</typeparam>
     /// <param name="services"></param>
     /// <param name="implementationFactory">The implementation factory for the specified type.</param>
-    public static void SwapTransient<TService>(this IServiceCollection services, Func<IServiceProvider, TService> implementationFactory)
+    public static IServiceCollection SwapTransient<TService>(this IServiceCollection services, Func<IServiceProvider, TService> implementationFactory)
     {
         services.RemoveTransient<TService>();
         services.AddTransient(typeof(TService), sp => implementationFactory(sp) ?? throw new InvalidOperationException());
+        return services;
     }
 
-    public static void SwapTransient<TService, TImplementation>(this IServiceCollection services)
+    public static IServiceCollection SwapTransient<TService, TImplementation>(this IServiceCollection services)
         where TImplementation : class, TService
         where TService : class
     {
         services.RemoveTransient<TService>();
         services.AddSingleton<TService, TImplementation>();
+        return services;
     }
 }
