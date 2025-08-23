aee7d6e800acf4824900eb5e70b2cf2e0fe88a2d
Kévin ROBERT <kevin.robert.kr@gmail.com>
2025-08-23 16:57:27 +0200

    Ajout PaginationDto

---
+++ src/Krosoft.Extensions.WebApi/Models/Dto/PaginationDto.cs
@@ -0,0 +1,20 @@
+﻿using Krosoft.Extensions.Core.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Krosoft.Extensions.WebApi.Models.Dto;
+
+public record PaginationDto : IPaginationDto
+{
+    [FromQuery]
+    public int? PageNumber { get; set; }
+
+    [FromQuery]
+    public int? PageSize { get; set; }
+
+    [FromQuery]
+    public string[]? SortBy { get; set; }
+
+    [FromQuery]
+    public string? Text { get; set; }
+}
+
