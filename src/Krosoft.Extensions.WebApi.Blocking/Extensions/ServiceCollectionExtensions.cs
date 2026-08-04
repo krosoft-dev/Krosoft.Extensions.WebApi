7b69e81d61c64017e760aecb470d5266f5c95e7e
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-08-04 12:19:38 +0200

    Passage aux options de blocage et montée de Krosoft.Extensions.Blocking

--- src/Krosoft.Extensions.WebApi.Blocking/Extensions/ServiceCollectionExtensions.cs
+++ src/Krosoft.Extensions.WebApi.Blocking/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Krosoft.Extensions.Blocking.Abstractions.Interfaces;
+using Krosoft.Extensions.Blocking.Abstractions.Interfaces;
+using Krosoft.Extensions.Blocking.Abstractions.Models;
 using Krosoft.Extensions.Blocking.Extensions;
 using Krosoft.Extensions.Blocking.Services;
 using Krosoft.Extensions.WebApi.Blocking.Middlewares;
@@ -9,9 +10,10 @@ namespace Krosoft.Extensions.WebApi.Blocking.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddWepApiBlocking(this IServiceCollection services)
+    public static IServiceCollection AddWepApiBlocking(this IServiceCollection services,
+                                                       Action<KrosoftBlockingOptions> action)
     {
-        services.AddBlocking();
+        services.AddBlocking(action);
         services.AddAccessTokenProvider();
         services.AddIdentifierProvider();
         services.AddTransient<BlockingMiddleware>();
@@ -23,8 +25,10 @@ public static class ServiceCollectionExtensions
     ///     Enregistre le blocage des seuls access tokens, sans les blocages par IP et par identifiant.
     ///     À utiliser avec <c>UseAccessTokenBlocking</c>.
     /// </summary>
-    public static IServiceCollection AddAccessTokenBlocking(this IServiceCollection services)
+    public static IServiceCollection AddAccessTokenBlocking(this IServiceCollection services,
+                                                            Action<KrosoftBlockingOptions> action)
     {
+        services.AddBlockingStorage(action);
         services.AddAccessTokenProvider();
         services.AddSingleton<IAccessTokenBlockingService, AccessTokenBlockingService>();
         services.AddTransient<AccessTokenBlockingMiddleware>();
